65cfce3f58618d5e17c2d82f855996dfb7683902
coder <coder@gmail.com>
2025-06-15 20:08:45 +0300

    Первый коммит

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+
+import sharp from "astro/assets/services/sharp";
+
+export default defineConfig({
+  base: "/",
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: "static",
+  image: {
+    service: sharp,
+  }
+});
---
+++ src/config.ts
@@ -0,0 +1,8 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'CherryEditor | Clothes Editor & Face Swap Bot';
+export const SITE_DESCRIPTION = 'Clothes Editor & Face Swap Bot';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
+export const TG_URL = "https://t.me/clothes_editor_bot";
