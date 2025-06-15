ca35c17c69b92c149d607f556e9b94b35ca1d2df
coder <coder@gmail.com>
2025-06-15 20:33:32 +0300

    p

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,13 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
-
 import sharp from "astro/assets/services/sharp";
 
 export default defineConfig({
-  base: "/",
+  base: "/site/",
   integrations: [mdx(), sitemap(), tailwind()],
   output: "static",
   image: {
     service: sharp,
-  }
+  },
 });
